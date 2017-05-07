e5772e8e417e9c82bba484c681c3f7eb302df04c
Eduardo <efsilvaa@gmail.com>
2017-05-07 16:02:28 -0300

    First commit

---
+++ ML Basico template.R
@@ -0,0 +1,36 @@
+# Projeto de ML em R Template Basico ----
+
+# 1. Crie um projeto em R ----
+# a) Crie e organize o diretórios
+# b) Use esse script como base
+
+# 2. Prepare o Problema ----
+# a) Carregue os pacotes necessários
+# b) Carregue os dados
+# c) Separe os dados para validação
+
+# 3. Visão Geral Preliminar dos Dados ----
+# a) Estatísticas descritivas 
+# b) Visualização dos dados  
+
+# 4. Preparação dos Dados ----
+# a) Limpeza dos dados
+# b) Seleção de atributos (features)
+# c) Aplicação de transformações
+
+# 5. Avaliação do Modelos/Algoritmos ----
+# a) Defina as opções validar e testar seu modelo
+#    (ex. cross validation)
+# b) Métrica de avaliação do modelo
+# c) Selecione um conjunto de modelos/algoritmos de ML 
+# d) Compare os modelos/algoritmos
+
+# 6. Refinamento do modelo ----
+# a) Otimize os parâmetros dos modelos/Algoritmos
+# b) Combine as previsões do modelos/algoritmos selecionados
+
+# 7. Finalize o Modelo ----
+# a) Predictions on validation dataset
+# b) Create standalone model on entire training dataset
+# c) Salvar o modelo para uso final
+# d) Gerar dos relatórios
